46c49d7fa3cafeb60c740dbb0d6d8c460c220ff7
Alexander Kalaj <unixdisciple.us@gmail.com>
2025-04-09 16:01:40 -0700

    Initial Commit

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "sshkey" {
+  description = "SSH key to use for the EC2 instances"
+  type        = string
+}
+
+variable "minio_license" {
+  description = "SSH key to use for the EC2 instances"
+  type        = string
+}
