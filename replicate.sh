cf1eca5aa4640ac01eb1abe88e157bbe7408874d
Alexander Kalaj <unixdisciple.us@gmail.com>
2025-04-14 21:11:48 -0700

    MinIO bootstrap destroy 2

---
+++ replicate.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Set the AWS region to us-west-2 for all AWS CLI commands
+AWS_REGION="us-west-2"
+
+# Retrieve the public IP address for each MinIO instance
+MINIO1=$(aws ec2 describe-instances --region "$AWS_REGION" \
+    --filters "Name=tag:Name,Values=minio-us-west-2-cluster1-1" \
+    --query "Reservations[*].Instances[*].PublicIpAddress" \
+    --output text)
+
+MINIO2=$(aws ec2 describe-instances --region "$AWS_REGION" \
+    --filters "Name=tag:Name,Values=minio-us-west-2-cluster2-1" \
+    --query "Reservations[*].Instances[*].PublicIpAddress" \
+    --output text)
+
+MINIO3=$(aws ec2 describe-instances --region "$AWS_REGION" \
+    --filters "Name=tag:Name,Values=minio-us-west-2-cluster3-1" \
+    --query "Reservations[*].Instances[*].PublicIpAddress" \
+    --output text)
+
+# Set the minio aliases using the 'mc alias set' command.
+mc alias set minio1 http://$MINIO1:9000 miniominio miniominio
+mc alias set minio2 http://$MINIO2:9000 miniominio miniominio
+mc alias set minio3 http://$MINIO3:9000 miniominio miniominio
+
+# Add the replication configuration using the 'mc admin replicate add' command.
+mc admin replicate add minio1 minio2 minio3
